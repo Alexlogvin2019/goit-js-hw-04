a4749ef04b6479ae960d00e268a642304e6fc04f
Alexlogvin2019 <alexlogvin@yahoo.com>
2020-01-23 16:55:58 +0200

    ver2

--- task-02.js
+++ task-02.js
@@ -4,18 +4,18 @@ const inventory = {
     add(itemName) {
       console.log(`Adding ${itemName} to inventory`);
   
-      inventory.items.push(itemName);
+      this.items.push(itemName);
     },
     remove(itemName) {
       console.log(`Removing ${itemName} from inventory`);
   
-      inventory.items = inventory.items.filter(item => item !== itemName);
+      this.items = this.items.filter(item => item !== itemName);
     },
   };
   
-  const invokeInventoryAction = function(itemName, action) {
+  const invokeInventoryAction = function(itemName, intaction) {
     console.log(`Invoking action on ${itemName}`);
-    action(itemName);
+    intaction.call(inventory, itemName);
   };
   
   invokeInventoryAction('Medkit', inventory.add);
@@ -24,7 +24,7 @@ const inventory = {
   
   console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
   
-  invokeInventoryAction('Gas mask', inventory.remove);
+ invokeInventoryAction('Gas mask', inventory.remove);
   // Invoking action on Gas mask
   // Removing Gas mask from inventory
   
